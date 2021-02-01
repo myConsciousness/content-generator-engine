fdde006247c2fe10927d6ee4b9b6a2da5021b6ab
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-01 19:40:46 +0900

    add

---
+++ app/src/main/kotlin/org/thinkit/generator/content/engine/catalog/Bracket.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.generator.content.engine.catalog
+
+import org.thinkit.api.catalog.BiCatalog
+
+/**
+ * ブラケットを管理するカタログです。
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
+internal enum class Bracket(private val code: Int, private val tag: String) :
+        BiCatalog<Bracket, String> {
+
+    /** 開始ブラケット */
+    START(0, "["),
+
+    /** 終了ブラケット */
+    END(1, "]");
+
+    override fun getCode(): Int = this.code
+
+    override fun getTag(): String = this.tag
+}
